a41e226c016b00a07ca906a94576106aafeb10e8
Marc Skov Madsen <masma@orsted.dk>
2022-11-14 05:57:09 +0100

    'Initial commit'

---
+++ tests/test_hello.py
@@ -0,0 +1,10 @@
+"""A Basic test file"""
+from budapestbi2022 import VERSION
+
+
+def test_hello():
+    """A basic test function"""
+    # Given nothing
+    # When
+    print(f"hello {VERSION}")
+    # Then no errors
